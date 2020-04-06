4d0900aa30da48b7873f481631498a65e0736b7f
DangTrungHieu <>
2020-04-06 09:59:23 +0700

    create source

---
+++ CinemaHub/CinemaHub/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  CinemaHub
+//
+//  Created by DangTrungHieu on 4/6/20.
+//  Copyright © 2020 DangTrungHieu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
---
+++ CinemaHub/CinemaHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CinemaHub
+//
+//  Created by DangTrungHieu on 4/6/20.
+//  Copyright © 2020 DangTrungHieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
