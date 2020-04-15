83e70584f5d74f10ee49a8d4b37adc09b4ab2466
DangTrungHieu <>
2020-04-15 15:12:55 +0700

    create code

--- CinemaHub/CinemaHub/ViewController.swift
+++ CinemaHub/CinemaHub/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let animateButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.setTitle("Animate", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.addTarget(self, action: #selector(animateButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        layoutAnimateButton()
     }
-
-
+    
+    @objc private func animateButtonPressed() {
+        
+        for _ in 0...10 {
+            
+            let duration : TimeInterval = 1.0
+            let delay    : TimeInterval = TimeInterval((900 + arc4random_uniform(100)) / 1000)
+            let options = UIView.AnimationOptions.curveLinear
+            
+            let size : CGFloat = CGFloat( arc4random_uniform(40))+20
+            let yPosition : CGFloat = CGFloat( arc4random_uniform(200)) + 20
+            
+            let apple = UIImageView()
+            apple.frame = CGRect(x: 0 - size, y: yPosition, width: size, height: size)
+            apple.image = UIImage(named: "logo")
+            self.view.addSubview(apple)
+            
+            UIView.animate(withDuration: duration, delay: delay, options: options, animations: {
+                apple.frame = CGRect(x: 320, y: yPosition, width: size, height: size)
+                
+            }, completion: { animationFinished in
+                
+                apple.removeFromSuperview()
+                
+            })
+        }
+    }
+    
+    private func layoutAnimateButton() {
+        view.addSubview(animateButton)
+        animateButton.snp.makeConstraints { (make) in
+            make.bottom.equalToSuperview().offset(-15)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(50)
+        }
+    }
+    
 }
 
---
+++ CinemaHub/Pods/Target Support Files/Pods-CinemaHub/Pods-CinemaHub-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CinemaHub : NSObject
+@end
+@implementation PodsDummy_Pods_CinemaHub
+@end
